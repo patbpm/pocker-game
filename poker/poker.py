c205376ec93796cbcc061b011c92723bb9814529
Patrick Bope <patbpm1@gmail.com>
2021-05-25 10:14:35 +0200

    Update testing for evaulation'

--- poker/poker.py
+++ poker/poker.py
@@ -1,5 +1,5 @@
 #hand = input("What card do you have in the Hand? ")
-hand = 'AS,10C,10H,3D,3S'
+hand = '10S,10C,10H,3D,3S'
 
 def evaluate_hands(str):
 
@@ -8,18 +8,44 @@ def evaluate_hands(str):
     letters = [hand[i][1:] for i in range(5)] 
     # The number for each card in the hand
     numbers = [hand[i][:-1] for i in range(5)]  
-    
+    ranks = list(map(converter, numbers))
 
     # Repetitions count for each number
     rnum = [numbers.count(i) for i in numbers] 
 
     # Repetitions count for each letter 
-    rlet = [letters.count(i) for i in letters]  
-    return numbers
+    rlet = [letters.count(i) for i in letters]
+    #return rnum 
     
-def converter(number):
+    if rnum.count(2) == 4:
+        return 'two pair'
+    elif 3 in rnum:
+        if rnum.count(3) == 3 and rnum.count(2) == 2:
+            return 'full house'
+        else:
+            return 'three of a kind'
+    elif 4 in rnum:
+        return 'four of a kind'
+    elif sorted(rnum) == [2,2,3,3,3]:
+        return 'full house'
+    elif rnum.count(2) == 2:
+        return 'pair'
+    else:
+        return 'High Card'
+   
+   
+
+def converter(num):
     if num == 'A':
         return 14
-    if
+    elif num == 'K':
+        return 13
+    elif num == 'Q':
+        return 12
+    elif num == 'J':
+        return 11
+    else:
+        return int(num)
+    
 
-print(evaluate_hands(hand))
+#print(evaluate_hands(hand))
--- test/test_poker_evaluation.py
+++ test/test_poker_evaluation.py
@@ -8,9 +8,41 @@ class TestPokerHand(unittest.TestCase):
         """
         Test that can evaluate two pairs
         """
-        data = '1S, 10C, 10H, 3D, 3S'
+        data = 'AS, 10C, 10H, 3D, 3S'
         result = evaluate_hands(data)
         self.assertEqual(result, 'two pair')
+    
+    def test_pairs(self):
+        """
+        Test that can evaluate pairs
+        """
+        data = 'AS, 10C, 10H,JD, 3S'
+        result = evaluate_hands(data)
+        self.assertEqual(result, 'pair')
+    
+    def test_three_of_a_kind(self):
+        """
+        Test that can evaluate three of a kind
+        """
+        data = 'AS,AC,AH,JD,3S'
+        result = evaluate_hands(data)
+        self.assertEqual(result, 'three of a kind')
+    
+    def test_four_of_a_kind(self):
+        """
+        Test that can evaluate four of a kind
+        """
+        data = 'AS,AC,AH,AD,3S'
+        result = evaluate_hands(data)
+        self.assertEqual(result, 'four of a kind')
+    
+    def test_full_house(self):
+        """
+        Test that can evaluate full house
+        """
+        data = 'AS,AC,AH,3D,3S'
+        result = evaluate_hands(data)
+        self.assertEqual(result, 'full house')
 
 if __name__ == '__main__':
     unittest.main()
