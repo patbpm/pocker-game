cc1fdc41a5db16c65b9fa3f2f593f696fc1978a6
Patrick Bope <patbpm1@gmail.com>
2021-05-25 13:26:25 +0200

    Raise error when wrong imput rank or suits

--- poker/poker.py
+++ poker/poker.py
@@ -1,51 +1,88 @@
-#hand = input("What card do you have in the Hand? ")
-hand = '10S,10C,10H,3D,3S'
+class PokerHand:
+    RANKS = {
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "10",
+        11: "J",
+        12: "Q",
+        13: "K",
+        14: "A",
+    }
+    SUITS = [
+         "H",  # Hearts
+         "D",  # Diamonds
+         "C",  # Clubs
+         "S",  # Spades
+    ]
+    def __init__(self, cards):
+        self.hand = cards
+        hand = self.hand.split(",")
+        # The suit for each card in the hand
+        self.letters = [hand[i][-1:] for i in range(5)] 
+        # The number for each card in the hand
+        numbers = [hand[i][:-1] for i in range(5)]  
+        self.ranks = list(map(self.converter, numbers))
 
-def evaluate_hands(str):
+        for index in range(len(hand)):
+            if self.ranks[index] not in PokerHand.RANKS:
+                raise ValueError("Invalid card rank")
+            if self.letters[index] not in PokerHand.SUITS:
+                raise ValueError("Invalid card suit")
+        
+        
 
-    hand = str.split(",")
-    # The suit for each card in the hand
-    letters = [hand[i][1:] for i in range(5)] 
-    # The number for each card in the hand
-    numbers = [hand[i][:-1] for i in range(5)]  
-    ranks = list(map(converter, numbers))
 
-    # Repetitions count for each number
-    rnum = [numbers.count(i) for i in numbers] 
+    def evaluate_hands( self):
 
-    # Repetitions count for each letter 
-    rlet = [letters.count(i) for i in letters]
-    #return rnum 
-    
-    if rnum.count(2) == 4:
-        return 'two pair'
-    elif 3 in rnum:
-        if rnum.count(3) == 3 and rnum.count(2) == 2:
+        # Repetitions count for each number
+        rnum = [self.ranks.count(i) for i in self.ranks] 
+
+        # Repetitions count for each letter 
+        rlet = [self.letters.count(i) for i in self.letters]
+        #return rnum 
+        
+        if rnum.count(2) == 4:
+            return 'two pair'
+        elif 3 in rnum:
+            if rnum.count(3) == 3 and rnum.count(2) == 2:
+                return 'full house'
+            else:
+                return 'three of a kind'
+        elif 4 in rnum:
+            return 'four of a kind'
+        elif sorted(rnum) == [2,2,3,3,3]:
             return 'full house'
+        elif rnum.count(2) == 2:
+            return 'pair'
         else:
-            return 'three of a kind'
-    elif 4 in rnum:
-        return 'four of a kind'
-    elif sorted(rnum) == [2,2,3,3,3]:
-        return 'full house'
-    elif rnum.count(2) == 2:
-        return 'pair'
-    else:
-        return 'High Card'
-   
-   
-
-def converter(num):
-    if num == 'A':
-        return 14
-    elif num == 'K':
-        return 13
-    elif num == 'Q':
-        return 12
-    elif num == 'J':
-        return 11
-    else:
-        return int(num)
+            return 'High Card'
+    
     
 
+    def converter(self, num):
+        if num == 'A':
+            return 14
+        elif num == 'K':
+            return 13
+        elif num == 'Q':
+            return 12
+        elif num == 'J':
+            return 11
+        else:
+            return int(num)
+    
+    def verification(self, ):
+        if num == 'A':
+            return 14
+        
+#hand = input("What card do you have in the Hand? ")
+hand = '10S,AC,10D,3D,3S'
 #print(evaluate_hands(hand))
+poker = PokerHand(hand)
+print(poker.evaluate_hands())
--- test/test_poker_evaluation.py
+++ test/test_poker_evaluation.py
@@ -1,6 +1,6 @@
 import unittest
 
-from poker.poker import *
+from poker.poker import PokerHand
 
 
 class TestPokerHand(unittest.TestCase):
@@ -9,7 +9,8 @@ class TestPokerHand(unittest.TestCase):
         Test that can evaluate two pairs
         """
         data = 'AS, 10C, 10H, 3D, 3S'
-        result = evaluate_hands(data)
+        poker = PokerHand(data)
+        result = poker.evaluate_hands()
         self.assertEqual(result, 'two pair')
     
     def test_pairs(self):
@@ -17,7 +18,8 @@ class TestPokerHand(unittest.TestCase):
         Test that can evaluate pairs
         """
         data = 'AS, 10C, 10H,JD, 3S'
-        result = evaluate_hands(data)
+        poker = PokerHand(data)
+        result = poker.evaluate_hands()
         self.assertEqual(result, 'pair')
     
     def test_three_of_a_kind(self):
@@ -25,7 +27,8 @@ class TestPokerHand(unittest.TestCase):
         Test that can evaluate three of a kind
         """
         data = 'AS,AC,AH,JD,3S'
-        result = evaluate_hands(data)
+        poker = PokerHand(data)
+        result = poker.evaluate_hands()
         self.assertEqual(result, 'three of a kind')
     
     def test_four_of_a_kind(self):
@@ -33,7 +36,8 @@ class TestPokerHand(unittest.TestCase):
         Test that can evaluate four of a kind
         """
         data = 'AS,AC,AH,AD,3S'
-        result = evaluate_hands(data)
+        poker = PokerHand(data)
+        result = poker.evaluate_hands()
         self.assertEqual(result, 'four of a kind')
     
     def test_full_house(self):
@@ -41,8 +45,11 @@ class TestPokerHand(unittest.TestCase):
         Test that can evaluate full house
         """
         data = 'AS,AC,AH,3D,3S'
-        result = evaluate_hands(data)
+        poker = PokerHand(data)
+        result = poker.evaluate_hands()
         self.assertEqual(result, 'full house')
+
+    
 
 if __name__ == '__main__':
     unittest.main()
